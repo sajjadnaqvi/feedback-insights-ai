d9bc706b1a6354cf3d5d5253f90df05a684fd3b0
Sajjad Naqvi <sajjadmehdi1214@gmail.com>
2025-06-05 09:32:07 +0500

    handle admin and user redirection

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -46,7 +46,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
+            return Auth::user()->is_admin ? redirect()->route('feedback.index') : redirect()->route('feedback.give');
         }
 
         return back()->withErrors([
